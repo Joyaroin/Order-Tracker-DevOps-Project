2e0571ee7e8aedf69f4d40b6b54776a5fa21a695
Joyaroin <adhamseddik546@gmail.com>
2026-04-16 13:22:50 -0400

    apatch

--- infra/terraform/ecr.tf
+++ infra/terraform/ecr.tf
@@ -9,6 +9,7 @@ resource "aws_ecr_repository" "app" {
 
   name                 = each.value
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
